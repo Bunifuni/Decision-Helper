62a0e67d80ca3d63b593b1cd302441126e36877c
jbinger <julian.binger@stud.htwk-leipzig.de>
2024-07-10 13:41:57 +0200

    variables for priority of correct seminar group and repeater

--- InputStruct.hpp
+++ InputStruct.hpp
@@ -9,6 +9,8 @@ struct InputStruct {
     
     bool verbose = false;
     uint8_t preferredPoints = 0;
+    uint8_t priorityCorrectSemGroup = 2;
+    uint8_t priorityRepeater = 1;
     std::string semGroup = "";
 
     std::map<int, std::set<std::string>> studSelection;
