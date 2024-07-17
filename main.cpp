e0f11ff0975591080ce4e6c59ccda1a87ccb6381
jbinger <julian.binger@stud.htwk-leipzig.de>
2024-07-17 10:41:00 +0200

    added distinguishing preprocessing command

--- main.cpp
+++ main.cpp
@@ -8,9 +8,23 @@ int main(int argc, char *argv[])
     if (preprocessing(argc, argv, &input) == -1)
         exit(-1);
     DescisionPipeline decider(&input);
+    Student *chosenOne;
+    switch (input.state)
+    {
+    case decision:
+        chosenOne = decider.decideForStudent();
+        if (chosenOne)
+            std::cout << "The chosen student is: \t" << chosenOne->getName() << std::endl;
+        break;
+    case increment:
+        decider.incrementPointsOfSelection();
+        break;
+    case decrement:
+        decider.decrementPointsOfSelection();
+        break;
+    default:
+        puts("ERROR: command unhandled");
+    }
 
-    Student *chosenOne = decider.decideForStudent();
-    if (chosenOne)
-        std::cout << "The chosen student is: \t" << chosenOne->getName() << std::endl;
     return 0;
 }
